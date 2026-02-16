ac1dbafd74dd6a298679f4a5172f06ebec7001c2
vaishnavik <vaishnavikbhaskar@gmail.com>
2026-02-16 07:06:11 +0000

    fix:add DuplicateEntryError in exception handiling

--- petropipe/crud_events/job_card_master.py
+++ petropipe/crud_events/job_card_master.py
@@ -1,10 +1,11 @@
 import frappe
+from frappe.exceptions import DuplicateEntryError
 
 def auto_job_card(doc, method=None):
     if not doc.job_no:
         return
     
-    job_card_name = frappe.db.get_value("Job Card Master", {"job_no": doc.job_no}, "name")
+    job_card_name = frappe.db.exists("Job Card Master",{"job_no": doc.job_no})
     if job_card_name:
         try:
             frappe.db.set_value("Job Card Master", job_card_name, {
@@ -20,10 +21,20 @@ def auto_job_card(doc, method=None):
             job_card_master = frappe.new_doc("Job Card Master")
             job_card_master.sales_order = doc.name
             job_card_master.job_no = doc.job_no
-            job_card_master.customer = doc.customer or ''
-            job_card_master.incoterm = doc.incoterm or ''
-            job_card_master.company = doc.company or ''
+            job_card_master.customer = doc.customer or ""
+            job_card_master.incoterm = doc.incoterm or ""
+            job_card_master.company = doc.company or ""
             job_card_master.insert(ignore_permissions=True)
+        except DuplicateEntryError:
+            existing_name = frappe.db.get_value( "Job Card Master",{"job_no": doc.job_no},"name")
+            if existing_name:
+                frappe.db.set_value("Job Card Master", existing_name, {
+                    "customer": doc.customer or "",
+                    "incoterm": doc.incoterm or "",
+                    "sales_order": doc.name or "",
+                    "company": doc.company or ""
+                }, update_modified=True)
+
         except Exception:
-            frappe.log_error(frappe.get_traceback(), "Job Card Master Creation Failed")
-            
+            frappe.log_error(frappe.get_traceback(),"Job Card Master Creation Failed")
+             
